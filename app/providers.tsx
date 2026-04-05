525ca1004b86faa8f7c2745f247302284487394d
Sharan Lankala <lankalasharan@gmail.com>
2026-04-06 00:53:16 +0200

    Remove dark mode, enforce light mode for all users

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,12 +2,11 @@
 
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
-import { DarkModeProvider } from "@/app/context/DarkModeContext";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <SessionProvider>
-      <DarkModeProvider>{children}</DarkModeProvider>
+      {children}
     </SessionProvider>
   );
 }
